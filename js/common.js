a89122f8f3b800372409ddd77526660b84f7758e
yjoh056 <yjoh056@gmail.com>
2023-09-04 17:07:24 +0900

    상단업데이트0903

--- js/common.js
+++ js/common.js
@@ -1,12 +1,20 @@
-<script>
-        const depth1 = document.querySelector('.dept1 li');
-        depth1.addEventListener('mouseover', function () {
-            depth1.querySelectorAll('.dept2').forEach((o) => { o.style.display = 'block' }) /* textMenu */
-        })
-        // hoverOn
-        depth1.addEventListener('mouseout', function () {
-            depth1.querySelectorAll('.dept2').forEach((o) => { o.style.display = 'none' }) /* textMenu */
-           
-        })
-        // hoverOff
-    </script>
+const depth1 = document.querySelector('.dept1 li');
+depth1.addEventListener('mouseover', function () {
+    depth1.querySelectorAll('.dept2').forEach((o) => { o.style.display = 'block' }) /* textMenu */
+})
+// hoverOn
+depth1.addEventListener('mouseout', function () {
+    depth1.querySelectorAll('.dept2').forEach((o) => { o.style.display = 'none' }) /* textMenu */
+
+})
+// hoverOff
+
+// 버튼 클릭 이벤트 처리
+document.getElementById("cart").addEventListener("click", function () {
+    var cartDiv = document.querySelector(".cart_list");
+    if(cartDiv.style.display="none"){
+        cartDiv.style.display="block";
+    }else{
+        cartDiv.style.display="none";
+    }
+});
